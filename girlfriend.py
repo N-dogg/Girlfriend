428a0fe693a5a92576412a4143cafad8a1230a8a
N-dogg <43980002+N-dogg@users.noreply.github.com>
2019-09-10 20:26:49 +1000

    Add files via upload

---
+++ girlfriend.py
@@ -0,0 +1,23 @@
+"""
+Return the number of compatible people
+"""
+
+#variables for population
+population = None
+gender = None
+location = None
+age = None/population
+education = None
+attractive = None
+
+#additional variables
+reciprocal = None
+single = None
+match = None
+
+if __name__ == '__main__':
+    initial = population*gender*(location/population)*age*education*attractive
+    print('Initial estimation: ', round(initial,2))
+    refined = initial*reciprocal*single*match
+    print('Refined estimation: ', round(refined,2))
+    print('Percentage of location: ', refined/population*100,'%')
